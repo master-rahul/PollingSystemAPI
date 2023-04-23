b451dc6ff8e3642d9e5c71daeb6b19b6f52d26c8
Rahul Verma <master.rahulverma@gmail.com>
2023-04-23 20:52:24 +0530

    Add files via upload

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,10 @@
+// getting mongoose module
 const mongoose =  require('mongoose');
+// connecting mongoose with mongodb
 mongoose.connect('mongodb://localhost/pollingSystemAPI');
+// fetching mongoose connection instance
 const db = mongoose.connection;
+// to check the instance status
 db.on('error', console.error.bind(console, 'Error in Connecting to Db'));
 db.once('open', () => {console.log('Connected Successfully to Database')});
 module.exports = db;
--- config/passport-jwt.js
+++ config/passport-jwt.js
@@ -1,12 +1,17 @@
+// getting the passport module
 const passport = require('passport');
+// getting the passport-jwt module
 const JwtStrategy = require('passport-jwt').Strategy;
+// getting the passport-jwt module for extraction of jwt token
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
+// setting option for jwt token
 let options = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // auth header has the json web token so we can extract 
     secretOrKey: 'secret' // every encryption and decryption happens from this key
 };
 
+// authenticating the requesting by verfying credentials
 passport.use(new JwtStrategy(options, function (jwtPayload, callback) {
     console.log("JWT PAYLOAD:::: ", jwtPayload);
     if(jwtPayload.user == 'admin' && jwtPayload.password == 'admin') {
